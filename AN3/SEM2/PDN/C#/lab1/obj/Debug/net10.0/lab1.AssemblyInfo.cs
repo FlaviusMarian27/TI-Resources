c670d062ef07c675a042ccfa8a5262e1fac0c019
FlaviusMarian27 <marianflavius27@yahoo.com>
2026-06-13 16:34:09 +0300

    Adaugare capitol TCP

--- AN3/SEM2/PDN/C#/lab1/obj/Debug/net10.0/lab1.AssemblyInfo.cs
+++ AN3/SEM2/PDN/C#/lab1/obj/Debug/net10.0/lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73d4239ebcb23d3f871a23a3cd8174c675d7f57d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88b1a4e4c80baff4ad8d7255eee57a6c27d0b2b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
